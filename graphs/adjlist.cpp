c1fcc575d842e66892065496893a6cac830f9e4c
Rahul Sonone <rahulsonone1234@gmail.com>
2020-08-17 15:29:32 +0530

    Update adjlist.cpp

--- graphs/adjlist.cpp
+++ graphs/adjlist.cpp
@@ -3,25 +3,24 @@ using namespace std;
 
 int main()
 {
-	int n,e;
-	cin>>n>>e;
-	vector<int>adj[n];
-	for(int i=0;i<e;i++)
-	{
-		int x,y;
-		cin>>x>>y;
-		adj[x].push_back(y);
-		adj[y].push_back(x);
-	}
-	for(int i=0;i<n;i++)
-	{
-	    cout<<i<<" :";
-		for(int j=0;j<adj[i].size();j++)
-		{
-			cout<<adj[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-	return 0;
-	
+    int n,e;
+    cin>>n>>e;
+    vector<int>adj[n];
+    for(int i=0;i<e;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    for(int i=1;i<n;i++)
+    {
+        cout<<i<<" :";
+        for(int j=0;j<adj[i].size();j++)
+        {
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
