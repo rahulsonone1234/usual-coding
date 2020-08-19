ddb600cbc49874183aad50f6e2bf0dcc753fe7a6
Rahul Sonone <rahulsonone1234@gmail.com>
2020-08-19 15:05:28 +0530

    Update dfs_recurrsive.cpp

--- graphs/dfs_recurrsive.cpp
+++ graphs/dfs_recurrsive.cpp
@@ -75,4 +75,42 @@ int main()
 	return 0;
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include<bits/stdc++.h>
+using namespace std;
 
+vector<vector<int>>adj(1000);
+int visited[1000001]={0};
+
+void dfs(int s)
+{
+    visited[s]=1;
+    cout<<s<<" ";
+   // vector<int>::iterator it;
+    for(auto it=adj[s].begin();it!=adj[s].end();it++)
+    {
+        if(!visited[*it])
+        {
+            dfs(*it);
+        }
+    }
+}
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    for(int i=0;i<e;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(visited[i]==0)
+        {
+            dfs(i);
+        }
+    }
+    return 0;
+}
