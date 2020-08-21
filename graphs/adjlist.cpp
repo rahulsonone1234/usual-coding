d9fee0d4111033b96060dce02dd4631dd701110f
Rahul Sonone <rahulsonone1234@gmail.com>
2020-08-21 21:23:39 +0530

    Update adjlist.cpp

--- graphs/adjlist.cpp
+++ graphs/adjlist.cpp
@@ -24,3 +24,32 @@ int main()
     }
     return 0;
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<pair<int, int>>adj[1000];
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    for(int i=0;i<e;i++)
+    {
+        int x,y,wt;
+        cin>>x>>y>>wt;
+        adj[x].push_back(make_pair(y, wt));
+        adj[y].push_back(make_pair(x, wt));
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<i<<" makes an edge with\n";
+        for(auto it=adj[i].begin();it!=adj[i].end();it++)
+        {   
+            cout<<it->first<<" having weight "<<it->second<<endl;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
