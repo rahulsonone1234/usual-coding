66ebff6b1a87d0e2a7e55eefabde1be90954904b
Rahul Sonone <rahulsonone1234@gmail.com>
2020-08-25 18:52:47 +0530

    Update Articulation points .cpp

--- graph imp/Articulation points .cpp
+++ graph imp/Articulation points .cpp
@@ -1,10 +1,4 @@
-/* 
-   Articulation points 
-   undirected graph 
-   connected graph 
-   nodes are numbered from 1 to n 
-*/ 
- 
+
 #include <bits/stdc++.h> 
 using namespace std; 
 #define endl '\n'; 
@@ -15,49 +9,34 @@ int parent [MXSIZE];
 int child  [MXSIZE]; 
 int low    [MXSIZE]; 
 int tym    [MXSIZE]; 
-bool seen  [MXSIZE]; 
+bool visited  [MXSIZE]; 
 bool is_cut[MXSIZE]; 
  
-vector <vector <int> > adj (MXSIZE); //adjancy graph 
+vector <vector <int> > adj (MXSIZE); 
  
 void dfs(int s) 
 { 
- 
- /* static variable gets declared only  
-    once till the program runs */ 
   static int c=0; 
- 
-  seen[s]=true;   // mark s as visited 
- 
-  low[s]=tym[s]=c++; // first assigns and then increment c. 
- 
+  visited[s]=true;  
+  low[s]=tym[s]=c++;  
  // checking all children of s 
   for(int i=0;i<adj[s].size();i++) 
   { 
      int curr = adj[s][i]; 
- 
      if(curr==parent[s]) 
       continue;         // do nothing if curr is its parent 
-   
-    if(!seen[curr]) 
+    if(!visited[curr]) 
     { 
- 
       child[s]++; // one more independent child of s 
       parent[curr]=s;  
       dfs(curr); 
- 
- 
-      /* the low time of the oldest ancestor 
-        reachable from both */ 
+      /* the low time of the oldest ancestor reachable from both */ 
       low[s] = min(low[s] , low[curr]);  
- 
- 
-     /* if curr has greater or equal  
-        low time than visited time of s then if we cut s then 
+      /* if curr has greater or equal
+      low time than visited time of s then if we cut s then 
         curr will get disconnected from the graph*/ 
       if(low[curr] >= tym[s]) 
         is_cut[s]=true; 
- 
     } 
     else 
     { 
@@ -65,26 +44,21 @@ void dfs(int s)
          tym[curr] , I have explained it below*/ 
       low[s]= min(low[s],tym[curr]); 
     } 
- 
- 
   } 
- 
 } 
  
 int main() 
 { 
   ios_base::sync_with_stdio(false);cin.tie(0);     
        
-      int n,m; 
-      cin>>n>>m;  // no. of vertices and edges 
- 
-      for(int i=1;i<=m;i++) 
+      int n,e; 
+      cin>>n>>e;  
+      for(int i=0;i<e;i++) 
       { 
-          int a,b; 
-          // undirected edge between a and b 
-          cin>>a>>b; 
-          adj[a].push_back(b); 
-          adj[b].push_back(a); 
+          int x,y;
+          cin>>x>>y; 
+          adj[x].push_back(y); 
+          adj[y].push_back(x); 
       } 
         
       parent[1]=-1; 
@@ -99,10 +73,9 @@ int main()
       for(int i=1;i<=n;i++) 
       { 
         if(is_cut[i]) 
-          cout<<i<<" is cut vertex"<<endl; 
+        {
+            cout<<i<<" is cut vertex"<<endl;   
+        }
       } 
- 
- 
-   
     return 0; 
 } 
