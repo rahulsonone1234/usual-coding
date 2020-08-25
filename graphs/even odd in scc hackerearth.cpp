c6fe8a55983b0e477fc7c80032e20af16de94d4f
Rahul Sonone <rahulsonone1234@gmail.com>
2020-08-25 10:18:43 +0530

    Update even odd in scc hackerearth.cpp

--- graphs/even odd in scc hackerearth.cpp
+++ graphs/even odd in scc hackerearth.cpp
@@ -1,3 +1,89 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>>adj(1000);
+vector<vector<int>>adjt(1000);
+int visited[100005]={0};
+stack<int>stk;
+int ocount=0;
+int ecount=0;
+
+int trandfs(int s)
+{
+    visited[s]=true;
+    cout<<s<<" ";
+    int count=1;
+    for(auto it:adjt[s])
+    {
+        if(!visited[it])
+        {
+            count+=trandfs(it);
+        }
+    }
+    return count;
+}
+
+void filldfs(int s)
+{
+    visited[s]=true;
+    for(auto it:adj[s])
+    {
+        if(!visited[it])
+        {
+            filldfs(it);
+        }
+    }
+    stk.push(s);
+}
+
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    for(int i=0;i<e;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adjt[y].push_back(x);
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(!visited[i])
+        {
+            filldfs(i);
+        }
+    }
+    for(int i=0;i<100005;i++)
+    {
+        visited[i]=0;
+    }
+    int cnt=0;
+    while(!stk.empty())
+    {
+        int v=stk.top();
+        stk.pop();
+        if(!visited[v])
+        {
+            cnt++;
+            int count=trandfs(v);
+            if(count%2)
+            {
+                ocount+=count;
+            }
+            else
+            {
+                ecount+=count;
+            }
+            cout<<endl;
+        }
+    }
+    cout<<"Connected Components : "<<cnt<<endl;
+    cout<<"Oddcount : "<<ocount<<endl;
+    cout<<"Evencount : "<<ecount<<endl;
+    return 0;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #include <bits/stdc++.h>
 using namespace std;
 
