5e2a68173e13cde0369d8db771cb31e0b252921c
Rahul Sonone <rahulsonone1234@gmail.com>
2020-08-26 11:40:58 +0530

    Update cycle detection in un-directed graph using parent array .cpp

--- graphs/cycle detection in un-directed graph using parent array .cpp
+++ graphs/cycle detection in un-directed graph using parent array .cpp
@@ -49,3 +49,54 @@ int main()
     cout<<"NO"<<endl;
     return 0;
 }
+
+//////////////////////////////////////////////////////////////////////////////////////
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>>adj(1000);
+int visited[100001]={0};
+
+
+int dfs(int s, int parent)
+{
+    visited[s]=1;
+    for(auto it:adj[s])
+    {
+       if((!visited[it])&&dfs(it, s))
+       {
+               return 1;
+       }
+       else if(it!=parent)
+       {
+             return 1;   
+       }
+    }
+    return 0;
+}
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    for(int i=0;i<e;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(!visited[i])
+        {
+            if(dfs(i, -1))
+            {
+                cout<<"YES"<<endl;
+                return 0;   
+            }
+        }
+    }
+    cout<<"NO"<<endl;
+    return 0;
+}
+
