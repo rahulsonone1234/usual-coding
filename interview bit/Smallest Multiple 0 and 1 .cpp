e700e26506f5c1f80c794ff06694ef757fbcaae7
Rahul Sonone <rahulsonone1234@gmail.com>
2020-08-27 11:14:00 +0530

    Update Smallest Multiple 0 and 1 .cpp

--- interview bit/Smallest Multiple 0 and 1 .cpp
+++ interview bit/Smallest Multiple 0 and 1 .cpp
@@ -1,3 +1,59 @@
+if(A==0)
+        return "0";
+ 
+    int val[A+1], par[A+1], flag[A+1];
+    memset(val,-1,sizeof(val));
+    memset(par,-1,sizeof(par));
+    memset(flag,0,sizeof(flag));
+ 
+    queue<int> q;
+ 
+    q.push(1%A);
+    par[1%A] = -1;
+    val[1%A] = 1;
+    flag[1%A] = 1;
+ 
+    while(!q.empty())
+    {
+        int num = q.front();
+        q.pop();
+ 
+        if(num == 0)
+        {
+            string s = "";
+ 
+            while(num!=-1)
+            {
+                s.push_back(val[num] + '0');
+                num = par[num];
+            }
+ 
+            reverse(s.begin(), s.end());
+ 
+            return s;
+        }
+ 
+        int num0 = (num*10)%A;
+        int num1 = (num*10 + 1)%A;
+ 
+        if(flag[num0]==0)
+        {
+            flag[num0] = 1;
+            par[num0] = num;
+            val[num0] = 0;
+            q.push(num0);
+        }
+ 
+        if(flag[num1] == 0)
+        {
+            flag[num1] = 1;
+            par[num1] = num;
+            val[num1] = 1;
+            q.push(num1);
+        }
+ 
+    }
+///////////////////////////////////////////////////////////////////////////////////////////
 #include<bits/stdc++.h>
 using namespace std;
 
