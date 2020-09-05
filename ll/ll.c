a22ea903613195e529f132dcdd16e160979f61e6
Rahul Sonone <rahulsonone1234@gmail.com>
2020-09-05 07:50:54 +0530

    Update ll.c

--- ll/ll.c
+++ ll/ll.c
@@ -751,6 +751,36 @@ int intersectPoint(Node* head1, Node* head2)
 //         return NULL;
 
 }
+//merge sorted list iterative
+ListNode *mergetwolist(ListNode *l1, ListNode *l2)
+{
+	ListNode fake(-1);
+	ListNode  *last=&fake;
+	while(l1!=NULL && l2!=NULL)
+	{
+		if(l1->val < l2->val)
+		{
+			last->next=l1;
+			last=l1;
+			l1=l1->next;
+		}
+		else
+		{
+			last->next=l2;
+			last=l2;
+			l2=l2->next;
+		}
+	}
+	if(l1!=NULL)
+	{
+		last->next=l1;
+	}
+	if(l2!=NULL)
+	{
+		last->next=l2;
+	}
+	return fake.next;
+}
 //Merge sorted list recurrsion
 NODE * SortedMerge(NODE* a, NODE *b)
 {
