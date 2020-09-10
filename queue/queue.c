41610db765213c0b841b53e0729001d5a90eb20f
Rahul Sonone <rahulsonone1234@gmail.com>
2020-09-10 12:06:51 +0530

    Update queue.c

--- queue/queue.c
+++ queue/queue.c
@@ -158,3 +158,29 @@ string Solution::solve(string s) {
 }
 
 ////////////////////////////////////////////////////////////////
+//alternative for the first non-repeating character
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    
+    pair<int, int>arr[256];
+    for(int i=0;s[i];i++)
+    {
+        (arr[s[i]].first)++;
+        (arr[s[i]].second)=i;
+    }
+    
+    int res=INT_MAX;
+    for(int i=0;i<256;i++)
+    {
+        if(arr[i].first==1)
+        {
+            res=min(res, arr[i].second);
+        }
+    }
+    cout<<s[res]<<"at index"<<res<<endl;
+}
