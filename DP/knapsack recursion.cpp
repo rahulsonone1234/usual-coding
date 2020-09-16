686badd0b5646c456a8dd028338f9ad954c29c80
Rahul Sonone <rahulsonone1234@gmail.com>
2020-09-16 10:30:46 +0530

    Create knapsack recursion.cpp

---
+++ DP/knapsack recursion.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int max(int a, int b){return (a>b)?a:b;}
+
+int knapsack(int wt[], int val[], int n, int W)
+{
+    if(n==0 || W==0)
+    return 0;
+    
+    if(wt[n]>W)
+        knapsack(wt, val, n-1, W);
+    else
+        return max(val[n]+knapsack(wt, val, n-1, W-wt[n]), knapsack(wt, val, n-1, W));
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int wt[n];
+    int val[n];
+    int W;
+    for(int i=0;i<n;i++){cin>>wt[i];}
+    for(int i=0;i<n;i++){cin>>val[i];}
+    cin>>W;
+    cout<<"Max Profit :"<<knapsack(wt, val, n, W)<<endl;
+    return 0;
+}
