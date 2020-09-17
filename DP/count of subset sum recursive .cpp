696915c77cc6b2e03aecf821a2190080356d86f4
Rahul Sonone <rahulsonone1234@gmail.com>
2020-09-17 18:07:23 +0530

    Create count of subset sum recursive .cpp

---
+++ DP/count of subset sum recursive .cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int subsetsum(int a[], int n, int sum)
+{
+    if(sum==0)
+    return 1;
+    if(n==0)
+    return 0;
+    
+    if(a[n-1]<=sum)
+        return (subsetsum(a, n-1,sum-a[n-1])+subsetsum(a, n-1, sum));
+    else
+        return subsetsum(a, n-1, sum);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int sum;
+    cin>>sum;
+    
+    int ans=subsetsum(a, n, sum);
+    cout<<"Count of Subsets with given sum : "<<ans<<endl;
+    return 0;
+}
