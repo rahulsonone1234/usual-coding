08c8884347fc84b5914ba6e84d2bb9f78e9c695b
Rahul Sonone <rahulsonone1234@gmail.com>
2020-09-18 11:53:06 +0530

    Create count number of subset with given difference .cpp

---
+++ DP/count number of subset with given difference .cpp
@@ -0,0 +1,8 @@
+sum(s1) - sum(s2)=diff;
+sum(s1) + sum(s2)=sum(arr[]);
+
+2sum(s1)=diff+sum(arr[]);
+
+sum(s1)=(diff+sum(arr[]))/2;
+
+count the subset with the given value i.e. the calculated one (diff+sum(arr[]))/2
