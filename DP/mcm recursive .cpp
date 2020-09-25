68dbe4236dcc9915668f07d5287a5979d3652f80
Rahul Sonone <rahulsonone1234@gmail.com>
2020-09-25 15:01:24 +0530

    Create mcm recursive .cpp

---
+++ DP/mcm recursive .cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int mcmrecursive(int a[], int i, int j)
+{
+    if(i>=j)
+        return 0;
+        
+    int min=INT_MAX;
+    int k;
+    for(k=i;k<=j-1;k++)
+    {
+        int tmp=mcmrecursive(a,i,k)+mcmrecursive(a,k+1,j)+(a[i-1]*a[k]*a[j]);
+        
+        if(tmp<min)
+        {
+            min=tmp;
+        }
+    }
+    return min;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<mcmrecursive(a, 1, n-1)<<endl;
+    return 0;
+}
