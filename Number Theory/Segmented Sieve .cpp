6ebb015a0578d61a65b9ee9592861040a2017b61
Rahul Sonone <rahulsonone1234@gmail.com>
2020-10-07 11:18:06 +0530

    Create Segmented Sieve .cpp

---
+++ Number Theory/Segmented Sieve .cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void simple_sieve(vector<int>&primes, int n)
+{
+    bool a[n+1];
+    memset(a, false, sizeof(a));
+    
+    for(int i=2;i<=n;i++){
+        if(a[i]==false){
+            primes.push_back(i);
+            for(int j=i;j<n;j+=i){
+                a[j]=true;
+            }
+        }
+    }
+}
+int main(){
+    int l,r;
+    cin>>l>>r;
+    
+    vector<int>primes;//primes for square root of R
+    simple_sieve(primes, floor(sqrt(r))+1);
+    
+    int n=r-l+1;
+    bool a[n+1];
+    memset(a, false, sizeof(a));
+    
+    for(int i=0;i<primes.size();i++)
+    {
+        int tmp=floor(l/primes[i])*primes[i];
+        
+        if(tmp<l)
+            tmp+=primes[i];
+            
+        if(tmp==primes[i])
+            tmp+=primes[i];
+            
+        for(int j=tmp;j<=r;j+=primes[i])
+        {
+            a[j-l]=true;
+        }
+    }
+    for(int i=l;i<=r;i++)
+    {
+        if(!a[i-l])
+        {
+            cout<<i<<" ";
+        }
+    }
+ return 0;   
+    
+}
