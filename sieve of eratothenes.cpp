e6646ec82670763b541c82654852aed2d5c8cb98
Rahul Sonone <rahulsonone1234@gmail.com>
2020-10-07 10:42:59 +0530

    Update sieve of eratothenes.cpp

--- sieve of eratothenes.cpp
+++ sieve of eratothenes.cpp
@@ -9,12 +9,12 @@ void SieveOfEratosthenes(int n)
 	// all entries it as true. A value in prime[i] will 
 	// finally be false if i is Not a prime, else true. 
 	bool prime[n+1]; 
-	memset(prime, false, sizeof(prime)); 
+	memset(prime, true, sizeof(prime)); 
 
 	for (int i=2; i*i<=n; i++) 
 	{ 
 		// If prime[i] is not changed, then it is a prime 
-		if (prime[i] == false) 
+		if (prime[i] == true) 
 		{ 
 			// Update all multiples of i greater than or 
 			// equal to the square of it 
@@ -22,14 +22,14 @@ void SieveOfEratosthenes(int n)
 			// less than i^2 are already been marked. 
 			for (int j=i*i; j<=n; j += i) 
 			{
-			    prime[j] = true;   
+			    prime[j] = false;   
 			}
 		} 
 	} 
 
 	// Print all prime numbers 
 	for (int i=2; i<=n; i++){
-	    if (!prime[i]){
+	    if (prime[i]){
 	        cout << i << " ";   
 	    }
 	}
