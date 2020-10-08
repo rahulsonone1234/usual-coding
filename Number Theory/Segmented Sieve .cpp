0f07e17cb243a0c00048f39d45ed7d0b42c1d3cf
Rahul Sonone <rahulsonone1234@gmail.com>
2020-10-08 13:43:37 +0530

    Update Segmented Sieve .cpp

--- Number Theory/Segmented Sieve .cpp
+++ Number Theory/Segmented Sieve .cpp
@@ -6,12 +6,22 @@ void simple_sieve(vector<int>&primes, int n)
     bool a[n+1];
     memset(a, true, sizeof(a));
     
-    for(int i=2;i*i<n;i++){
-        if(a[i]==true){
-            primes.push_back(i);
-            for(int j=i*i;j<n;j+=i){
+    for(int i=2;i*i<=n;i++)
+    {
+        if(a[i]==true)
+        {
+        
+            for(int j=i*i;j<n;j+=i)
+            {
                 a[j]=false;
             }
+        }
+    }
+    for(int i=2;i<n;i++)
+    {
+        if(a[i])
+        {
+            primes.push_back(i);
         }
     }
 }
