1c914d3bd4d662a8d06f5c67d662e3a6a6e24a1e
Rahul Sonone <rahulsonone1234@gmail.com>
2020-10-09 18:30:06 +0530

    Create Primality Test .cpp

---
+++ Number Theory/Primality Test .cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isprime(int n)
+{
+    if(n==1)
+    return false;
+    
+    for(int i=2;i*i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(isprime(n))
+        {
+            cout<<"yes"<<endl;
+        }
+        else
+        {
+            cout<<"no"<<endl;
+        }
+    }
+    return 0;
+}
