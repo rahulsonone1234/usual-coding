c12e416ff9c61033889a0f460dee03672cc7194f
Rahul Sonone <rahulsonone1234@gmail.com>
2021-05-25 20:57:00 +0530

    Update djasklj.cpp

--- djasklj.cpp
+++ djasklj.cpp
@@ -737,5 +737,108 @@ public:
         
     }
 };
+//Word Break
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+    //try all the prefix combination and for every prefix check the corressponding suffix and if both exist then return true else false
+    // A recursive program to test whether a given
+// string can be segmented into space separated
+// words in dictionary
+#include <iostream>
+using namespace std;
+
+/* A utility function to check whether a word is
+present in dictionary or not. An array of strings
+is used for dictionary. Using array of strings for
+dictionary is definitely not a good idea. We have
+used for simplicity of the program*/
+int dictionaryContains(string word)
+{
+	string dictionary[] = {"mobile","samsung","sam","sung",
+							"man","mango","icecream","and",
+							"go","i","like","ice","cream"};
+	int size = sizeof(dictionary)/sizeof(dictionary[0]);
+	for (int i = 0; i < size; i++)
+		if (dictionary[i].compare(word) == 0)
+		return true;
+	return false;
+}
+
+// returns true if string can be segmented into space
+// separated words, otherwise returns false
+bool wordBreak(string str)
+{
+	int size = str.size();
+
+	// Base case
+	if (size == 0) return true;
+
+	// Try all prefixes of lengths from 1 to size
+	for (int i=1; i<=size; i++)
+	{
+		// The parameter for dictionaryContains is
+		// str.substr(0, i) which is prefix (of input
+		// string) of length 'i'. We first check whether
+		// current prefix is in dictionary. Then we
+		// recursively check for remaining string
+		// str.substr(i, size-i) which is suffix of
+		// length size-i
+		if (dictionaryContains( str.substr(0, i) ) &&
+			wordBreak( str.substr(i, size-i) ))
+			return true;
+	}
+
+	// If we have tried all prefixes and
+	// none of them worked
+	return false;
+}
+
+// Driver program to test above functions
+int main()
+{
+	wordBreak("ilikesamsung")? cout <<"Yes\n": cout << "No\n";
+	wordBreak("iiiiiiii")? cout <<"Yes\n": cout << "No\n";
+	wordBreak("")? cout <<"Yes\n": cout << "No\n";
+	wordBreak("ilikelikeimangoiii")? cout <<"Yes\n": cout << "No\n";
+	wordBreak("samsungandmango")? cout <<"Yes\n": cout << "No\n";
+	wordBreak("samsungandmangok")? cout <<"Yes\n": cout << "No\n";
+	return 0;
+}
+then further optmize it and solve subproblems and
+    //for dp solution insert all the dictionary words in set and then check for every letter ending at position i and then in next loop j will cehck for every word froming from j--> to i
+    class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+         int n =s.size();
+        
+        set<string> dict;
+        for(auto w: wordDict)
+            dict.insert(w);
+        
+        vector<bool> dp(n+1,false);
+        dp[0] = true; //You can make empty string with any wordDict
+        
+        //From every i position check if you can make word ending here
+        for(int i=0;i<n;i++)
+        {
+            //If we can make a word from j->i
+            //We need to check if we can make a word till j first
+            for(int j=i;j>=0;j--)
+            {
+                string curr = s.substr(j,i-j+1); //pos,noOfChars
+                
+                if(dict.find(curr)!=dict.end())
+                    dp[i+1] = dp[i+1] || dp[j]; //dp[j] means can be make words before j(dp is 1 indexed)
+                
+                if(dp[i+1])
+                    break;
+            }
+        }
+        
+        return dp[n];
+    }
+};
+
 
 
