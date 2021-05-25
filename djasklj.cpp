dad952414c7c0c04cdfb2042a46ac2ddf5fae6bf
Rahul Sonone <rahulsonone1234@gmail.com>
2021-05-25 17:45:17 +0530

    Update djasklj.cpp

--- djasklj.cpp
+++ djasklj.cpp
@@ -708,3 +708,34 @@ public:
     }
 };
 
+//Maximal Square // dp /// solve over the corners and store the min of all +1
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int rows=matrix.size();
+        if(rows==0) return 0;
+        
+        int cols=matrix[0].size();
+        
+        vector<vector<int>>dp(rows+1, vector<int>(cols+1, 0));
+        int largest=0;
+        for(int i=1;i<=rows;i++)
+        {
+            for(int j=1;j<=cols;j++)
+            {
+                if(matrix[i-1][j-1]=='1')
+                {
+                   dp[i][j]=1+min( dp[i-1][j-1],min( dp[i-1][j], dp[i][j-1]));
+                                 
+                    if(dp[i][j]>largest)
+                    largest=dp[i][j];
+                }
+            }
+        }
+                                 
+        return largest*largest;
+        
+    }
+};
+
+
