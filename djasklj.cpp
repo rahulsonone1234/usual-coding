1c029e0a37a643b015881e61ce2b087c83dda055
Rahul Sonone <rahulsonone1234@gmail.com>
2021-06-07 09:00:53 +0530

    Update djasklj.cpp

--- djasklj.cpp
+++ djasklj.cpp
@@ -295,61 +295,68 @@ public:
     }
     
     void addNum(int num) {
-        int lsize = maxheap.size();
-        int rsize = minheap.size();
-        if(lsize==0)    //num is the 1st element in stream
-            maxheap.push(num);
-        else if(lsize==rsize)   //Push one element in maxheap for sure
+        if(maxheap.size()==minheap.size())
         {
-            if(num<minheap.top())   //num can be pushed to maxheap (1st half) to maintain order
-                maxheap.push(num);
-            else    //Push root of minheap to maxheap (Push num to 2nd half)
+            if(maxheap.size()==0)
             {
-                int temp = minheap.top();   //Save root of minheap
-                minheap.pop();  //Pop the root from minheap
-                minheap.push(num);  //Push num in minheap
-                maxheap.push(temp); //Push the previous saved root of minheap in the maxheap
+                maxheap.push(num);
+            }
+            else if(num<maxheap.top())
+            {
+                maxheap.push(num);
+            }
+            else
+            {
+                minheap.push(num);
             }
         }
-        else    ///We assume that maxheap can be larger than minheap by a max of 1 element only
+        else
         {
-            if(minheap.size()==0)
+            if(maxheap.size() > minheap.size())
             {
-                if(num>maxheap.top()) //Push num to 2nd half
-                    minheap.push(num);
-                else //Push num to 1st half
+                if(num >=maxheap.top())
                 {
-                    int temp = maxheap.top();
+                    minheap.push(num);
+                }
+                else
+                {
+                    int tmp=maxheap.top();
                     maxheap.pop();
+                    minheap.push(tmp);
                     maxheap.push(num);
-                    minheap.push(temp);
                 }
             }
-            else if(num>=minheap.top()) //Push the element directly in minheap (2nd half)
-                minheap.push(num);
-            else    //Push root of maxheap to minheap
+            else
             {
-                if(num<maxheap.top())   //Push num to 1st half
+                if(num <=minheap.top())
                 {
-                    int temp = maxheap.top();   //Save root of maxheap
-                    maxheap.pop();  //Pop root of maxheap
-                    maxheap.push(num);  //Push num to maxheap
-                    minheap.push(temp); //Push previous saved root of maxheap to minheap
+                    maxheap.push(num);
                 }
-                else    //Push num to 2nd half
+                else
+                {
+                    int tmp=minheap.top();
+                    minheap.pop();
+                    maxheap.push(tmp);
                     minheap.push(num);
+                }
             }
         }
         
     }
     
     double findMedian() {
-         int lsize = maxheap.size();
-        int rsize = minheap.size();
-        if(lsize > rsize)  //Return top of maxheap for odd no of elements
-            return double(maxheap.top());
-        else    //Else return avg of top of maxheap and minheap
-            return (double(maxheap.top())+double(minheap.top()))/2;
+        if(minheap.size()==maxheap.size())
+        {
+            return (minheap.top()+maxheap.top())/2.0;
+        }
+        else if(maxheap.size()>minheap.size())
+        {
+            return maxheap.top();
+        }
+        else 
+        {
+            return minheap.top();
+        }
 
     }
 };
