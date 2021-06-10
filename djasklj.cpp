2bdc75bc396bf67c4291907ed915995f81573e1a
Rahul Sonone <rahulsonone1234@gmail.com>
2021-06-10 08:35:35 +0530

    Update djasklj.cpp

--- djasklj.cpp
+++ djasklj.cpp
@@ -1281,19 +1281,36 @@ public:
 	vector<int> ans;
 	vector<pair<int,int>> nums;
 
-	void getRevInv(int i1,int j1,int i2,int j2) {
-		int i=i1,c=0;
+	void getRevInv(int left,int mid,int right) 
+    	{
+		int i=left;
+        	int j=mid;
+        
 		vector<pair<int,int>> v;
-		while(i1<=j1 && i2<=j2) {
-			if(nums[i2].first>=nums[i1].first) v.push_back(nums[i2++]);
-			else {
-				v.push_back(nums[i1]);
-				ans[nums[i1++].second]+= j2-i2+1;
+        
+		while(i<=mid-1 && j<=right)
+        	{
+			if(nums[j].first>=nums[i].first) 
+            		{
+                		v.push_back(nums[j++]);   
+            		}
+			else 
+            		{
+				v.push_back(nums[i]);
+				ans[nums[i++].second]+= right-j+1;
 			}
 		}
-		while(i1<=j1) v.push_back(nums[i1++]);
-		while(i2<=j2) v.push_back(nums[i2++]);
-		for(auto j: v) nums[i++]=j;
+        
+		while(i<=mid-1) 
+            		v.push_back(nums[i++]);
+        
+        
+		while(j<=right) 
+            		v.push_back(nums[j++]);
+        
+        
+		for(auto x: v)
+            	nums[left++]=x;
 	}
 
 	void merge(int i,int j) {
@@ -1301,7 +1318,7 @@ public:
 		int mid= (i+j)/2;
 		if(i!=mid) merge(i,mid);
 		if(mid+1!=j) merge(mid+1,j);
-		getRevInv(i,mid,mid+1,j);
+		getRevInv(i,mid+1,j);
 	}
 
 	vector<int> countSmaller(vector<int>& arr) {
