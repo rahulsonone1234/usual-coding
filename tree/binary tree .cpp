985b6b230ec550d1be341322005822bd6edcbd66
Rahul Sonone <rahulsonone1234@gmail.com>
2021-06-13 10:16:35 +0530

    Update binary tree .cpp

--- tree/binary tree .cpp
+++ tree/binary tree .cpp
@@ -1118,3 +1118,84 @@ public:
          
      }
 };
+
+    //All Nodes At Distance K in binary tree
+    /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>v;
+    void findnodesinsubtree(TreeNode *root, int k)
+    {
+        if(root==NULL || k<0)
+            return ;
+        
+        if(k==0)
+        {
+            v.push_back(root->val);
+            return;
+        }
+        
+        findnodesinsubtree(root->left, k-1);
+        findnodesinsubtree(root->right, k-1);
+    }
+    
+    int distancetp(TreeNode* root, TreeNode* target, int k)
+    {
+         if(root==NULL)
+            return -1;
+        
+        if(root==target)
+        {
+            findnodesinsubtree(root, k);
+            return 0;
+        }
+        
+        int dl=distancetp(root->left, target, k);
+        if(dl!=-1)
+        {
+            if(dl+1==k)
+            {
+                v.push_back(root->val);
+            }
+            else
+            {
+                findnodesinsubtree(root->right, k-dl-2);
+            }
+            
+            return 1+dl;
+        }
+        
+        
+        int dr=distancetp(root->right, target, k);
+        if(dr!=-1)
+        {
+            if(dr+1==k)
+            {
+                v.push_back(root->val);
+            }
+            else
+            {
+                findnodesinsubtree(root->left, k-dr-2);
+            }
+            
+            return 1+dr;
+        }
+        
+        return -1;
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        
+       
+        distancetp(root, target, k);
+        return v;
+        
+    }
+};
